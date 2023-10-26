2248f5b1a973ba6d4ac9e5acc4fff7b912a25f98
Rafik <70471535+Rivo11-11@users.noreply.github.com>
2023-10-26 12:49:02 -0700

    Time: 22 ms (62.06%), Space: 16.1 MB (10.44%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,18 +4,40 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution(object):
     def maxDepth(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        def FindMaxD(root,count) :
-            if not root :
-                return 0
-            if not (root.right or root.left) :
-                return 1
-            count =1
-            count += max(FindMaxD(root.left,count ),FindMaxD(root.right,count ))
-            return count
-        return FindMaxD(root,0)
+        # DFS
+        # def FindMaxD(root,count) :
+        #     if not root :
+        #         return 0
+        #     if not (root.right or root.left) :
+        #         return 1
+        #     count =1
+        #     count += max(FindMaxD(root.left,count ),FindMaxD(root.right,count ))
+        #     return count
+        # return FindMaxD(root,0)
+        ## BFS
+        queue = deque() 
+        if not root :
+            return 0
+        queue.append(root)
+        level = 0
+        while queue :
+            ## take a snapshot from the queue
+            for i in range(len(queue)) :
+                node = queue.popleft()
+                if node.left :
+                 queue.append(node.left)
+                if node.right :
+                 queue.append(node.right)
+            level+=1
+        return level 
+
+                
+
+        

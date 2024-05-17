1a5effc90c5c422a68526caec0730ad357453959
Rafik <70471535+Rivo11-11@users.noreply.github.com>
2024-05-17 11:23:32 +0300

    Time: 34 ms (22.00%), Space: 12.4 MB (96.00%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
@@ -11,27 +11,37 @@ class Solution(object):
         :type target: int
         :rtype: TreeNode
         """
-        def dfs(root) :
-            if not root :
-                return True
-            if not root.left and not root.right :
-                if root.val == target :
-                    root.val = 0
-                    return True
-                return
-            val1 = dfs(root.left)
-            val2 = dfs(root.right)
-            if val1 and val2 :
-                    if root.val == target :
-                        root.val = 0
-                        return True
-        dfs(root)
-        def construct(root):
-            if not root or root.val == 0:
-                return None
-            new_root = TreeNode(root.val)
-            new_root.left = construct(root.left)
-            new_root.right = construct(root.right)
-            return new_root
+        # def dfs(root) :
+        #     if not root :
+        #         return True
+        #     if not root.left and not root.right :
+        #         if root.val == target :
+        #             root.val = 0
+        #             return True
+        #         return
+        #     val1 = dfs(root.left)
+        #     val2 = dfs(root.right)
+        #     if val1 and val2 :
+        #             if root.val == target :
+        #                 root.val = 0
+        #                 return True
+        # dfs(root)
+        # def construct(root):
+        #     if not root or root.val == 0:
+        #         return None
+        #     new_root = TreeNode(root.val)
+        #     new_root.left = construct(root.left)
+        #     new_root.right = construct(root.right)
+        #     return new_root
 
-        return construct(root)
+        # return construct(root)
+        def dfs(root):
+            if not root:
+                return None
+            root.left = dfs(root.left)
+            root.right = dfs(root.right)
+            if not root.left and not root.right and root.val == target:
+                return None
+            return root
+        return dfs(root)
+    

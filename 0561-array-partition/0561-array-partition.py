24edb71e0ac5c3ddb1734f08d3da56b877e87f8f
Rafik <70471535+Rivo11-11@users.noreply.github.com>
2024-08-25 17:27:33 +0300

    Time: 207 ms (41.24%), Space: 13.8 MB (41.52%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -5,28 +5,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # maxi = nums[0] 
-        # mini = nums[0]
-        # res = []
-        # mymap = defaultdict(int)
-        # n = len(nums)
-        # ## count frequencies
-        # for i in range(n) :
-        #     maxi = max(maxi,nums[i]) 
-        #     mini = min(mini,nums[i])
-        #     mymap[nums[i]] += 1
-
-        # for i in range(mini , maxi + 1) :
-        #     if i in mymap : 
-        #         res.extend([i]*mymap[i]) 
-        res = sorted(nums)
-        i = 1
+        nums.sort() 
+        n = len(nums)
         s = 0
-        for num in res :
-            ## if odd
-            if i % 2 :
-                s += num 
-            i+=1
+        for i in range(n) :
+            if i % 2 == 0: 
+                s += nums[i]
         return s
+
 
 
